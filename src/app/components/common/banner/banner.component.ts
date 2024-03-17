80f7e4d114877de1dd02d2add64d8a47498c3ab5
Muhammad Arslan <m.arslan2037@gmail.com>
2024-03-18 00:18:00 +0500

    banner is dynamic now for each page

--- src/app/components/common/banner/banner.component.ts
+++ src/app/components/common/banner/banner.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-banner',
-  templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+    selector: 'app-banner',
+    templateUrl: './banner.component.html',
+    styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent {
-
+    @Input() imgSection!: boolean;
+    @Input() homeBanner!: boolean;
+    @Input() bannerText!: string;
 }
